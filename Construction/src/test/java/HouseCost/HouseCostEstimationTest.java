9baea448aa9180e2aa8e339cae8c42568e7b0410
Manojkumar953 <60352929+Manojkumar953@users.noreply.github.com>
2020-02-17 15:29:22 +0530

    Add files via upload

---
+++ Construction/src/test/java/HouseCost/HouseCostEstimationTest.java
@@ -0,0 +1,31 @@
+package HouseCost;
+import static org.junit.Assert.assertEquals;
+
+//import static org.junit.Assert.assertTrue;
+
+//import org.junit.Test;
+
+/**
+ * author manojkumar
+ * Unit test for simple HouseCostEstimationTest.
+ */
+public class HouseCostEstimationTest 
+{
+    
+    public void shouldAnswerWithTrue()
+    {
+    	/* 
+    	 * test case for the house construction cost estimation
+    	 * 
+    	 */
+        HouseCostEstimation obj = new HouseCostEstimation() ;
+        
+        assertEquals(2400 , obj.calculateHouseConstrucionCost(true,false,false,false,2)) ; 
+        
+        assertEquals(3000 , obj.calculateHouseConstrucionCost(false,true,false,false,2)) ;
+        
+        assertEquals(3600 , obj.calculateHouseConstrucionCost(false,false,true,false,2)) ;
+        
+        assertEquals(5000 , obj.calculateHouseConstrucionCost(false,false,true,true,2)) ;
+    }
+}
